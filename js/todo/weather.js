faf6487c5619217e60577fe94f68347258db9579
Eyeless356 <95523501+Eyeless356@users.noreply.github.com>
2021-12-04 20:06:46 +0900

    js

---
+++ js/todo/weather.js
@@ -0,0 +1,23 @@
+const weather = document.querySelector("#weather span:first-child");
+const city = document.querySelector("#weather span:last-child");
+const API_KEY = "495a5726f958d438524d222d9d0d917c";
+
+function onGeoOk(position) {
+  const lat = position.coords.latitude;
+  const lon = position.coords.longitude;
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      city.innerText = data.name;
+      weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
+    });
+}
+function onGeoError() {
+  alert("Can't find you. No weather for you.");
+}
+
+navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError);
+
+// latitude: 37.6452841
+// longitude: 127.0144349
